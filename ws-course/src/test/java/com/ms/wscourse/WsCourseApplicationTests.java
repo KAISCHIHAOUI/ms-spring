fe9adb766361034dc461b5e534bf4e38bcfd57b7
KAISCHIHAOUI <kais.chihaoui@esprit.tn>
2023-10-18 11:13:28 +0100

    init boostraping

---
+++ ws-course/src/test/java/com/ms/wscourse/WsCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.wscourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
